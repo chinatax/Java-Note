e542baedf8f7ffcc86594468bba1faa233108e41
Van <1251787298@qq.com>
2019-01-10 00:01:22 +0800

    init

---
+++ Tool/src/main/java/cn/van/entity/UserEntity.java
@@ -0,0 +1,34 @@
+package cn.van.entity;
+
+public class UserEntity {
+
+    private Long id;
+
+    private String name;
+
+    private String mobile;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
